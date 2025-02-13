583ac16dfa76bdd8ea9117e9c604c1e6bbc72a59
Ncrite1 <145476140+Pigase@users.noreply.github.com>
2025-02-13 19:11:01 +0300

    meteorite destruction by bullets

--- Flight of fantasies/Assets/ScriptsGame/Meteor.cs
+++ Flight of fantasies/Assets/ScriptsGame/Meteor.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 using static UnityEngine.GraphicsBuffer;
 
 public class Meteor : MonoBehaviour
 {
+    [SerializeField] private float _maxHealth ;
+    [SerializeField] private TextMeshProUGUI _texthHealth ;
+
     private float _randomSpeed;
     private float _randomX;
     private Vector3 _target;
+    private float _currentHealth;
+
     private void Update()
     {
         Move();
@@ -19,12 +25,24 @@ public class Meteor : MonoBehaviour
         _randomSpeed = Random.Range(0.5f, 2);
         _randomX = Random.Range(-2f, 2f);
         _target = new Vector3(_randomX, -5.7f,0);
+        _currentHealth = _maxHealth;
+        _texthHealth.text = _currentHealth.ToString();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "MeteorDestroyer")
         gameObject.SetActive(false);
+        if (collision.tag == "Bullet")
+        {
+            _currentHealth -= 2;
+            _texthHealth.text = _currentHealth.ToString();
+
+            if (_currentHealth <= 0 )
+                gameObject.SetActive(false);
+
+            collision.gameObject.SetActive(false);
+        }
     }
 
     private void Move()
