5c2cf18bf4d804a9640fce5d6fbfc870da0979a1
FrolHub <artem.frolovich.00@mail.ru>
2025-02-20 22:23:24 +0300

    FixFonCodeCreateShopMenu

--- Flight of fantasies/Assets/ScriptsMenu/MoveFonMenu.cs
+++ Flight of fantasies/Assets/ScriptsMenu/MoveFonMenu.cs
@@ -7,20 +7,15 @@ using UnityEngine.UIElements;
 public class MoveFonMenu : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private float _restartPosY;
-    [SerializeField] private SpriteRenderer _sprite;
+    [SerializeField] private GameObject[] _sprite;
 
-    private float _minSizeSpriteY;
 
-    private Vector2 zeroVector = new Vector2(0,0);
-    private Vector2 spriteSize;
+    private float minPosY;
+    private float maxPosY;
 
     private void Start()
     {
-        transform.parent.position = zeroVector;
-        spriteSize = _sprite.bounds.size;
-        _minSizeSpriteY = spriteSize.y;
-        Debug.Log(_minSizeSpriteY);
+        CheckPos();
     }
     
     private void FixedUpdate()
@@ -31,13 +26,29 @@ public class MoveFonMenu : MonoBehaviour
     private void MoveFon()
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
-        if(transform.position.y <= 0 )
+
+       if(transform.position.y <= minPosY)
+       {
+            transform.position = new Vector2(transform.position.x, maxPosY);
+       }
+    }
+    private void CheckPos()
+    {
+        minPosY = transform.position.y;
+        maxPosY = transform.position.y;
+
+        for (int i = 0; i < _sprite.Length; i++)
         {
-            if (Vector2.Distance(zeroVector, transform.position) >= _minSizeSpriteY)
+            if (_sprite[i].transform.position.y > maxPosY) 
+            { 
+                maxPosY = _sprite[i].transform.position.y; 
+            } 
+            else 
             {
-                transform.position = new Vector2(transform.position.x, _restartPosY);
-                Debug.Log(_restartPosY);
+                minPosY = _sprite[i].transform.position.y;
             }
         }
+
+        minPosY -= maxPosY;
     }
 }
