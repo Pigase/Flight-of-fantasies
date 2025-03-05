3794b61ad97db869ac03bc5c2ac1ffdd33aaf552
FrolHub <artem.frolovich.00@mail.ru>
2025-03-05 21:36:04 +0300

    AddAnimationDestroyMeteorFixPusher

--- Flight of fantasies/Assets/ScriptsMenu/Purchaser.cs
+++ Flight of fantasies/Assets/ScriptsMenu/Purchaser.cs
@@ -12,21 +12,26 @@ public class Purchaser : MonoBehaviour
         {
             case "BuyingCrystalsOneCristal":
                 AddCrystal(500);
+                Debug.Log("Buy1");
                 break;
             case "BuyingCrystalsTwoCristal":
                 AddCrystal(1000);
+                Debug.Log("Buy2");
                 break;
             case "BuyingCrystalsThreeCristal":
                 AddCrystal(2000);
+                Debug.Log("Buy3");
                 break;
             case "BuyingCrystalsFourCristal":
                 AddCrystal(4000);
+                Debug.Log("Buy4");
                 break;
         }
     }
 
     private void AddCrystal(float crystalsCount)
     {
+
         float crystal = PlayerPrefs.GetFloat("Crystals");
         crystal += crystalsCount;
         PlayerPrefs.SetFloat("Crystals", crystal);
